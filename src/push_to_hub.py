e54e0fb55f40a2e19b67e6282ebe8d8bda01dd4d
Nam Fam <102389548+namwasinyourheart@users.noreply.github.com>
2025-03-15 11:21:04 +0700

    src:upate push_to_hub.py

--- src/push_to_hub.py
+++ src/push_to_hub.py
@@ -5,12 +5,12 @@ from utils.exp_utils import setup_environment
 
 setup_environment()
 
-def push_to_hub(repo_name: str, trained_model_path: str, push_model: bool = False, push_tokenizer: bool = False):
+def push_to_hub(repo_id: str, trained_model_path: str, push_model: bool = False, push_tokenizer: bool = False):
     """
     Pushes a fine-tuned model and/or its tokenizer to the Hugging Face Hub.
 
     Args:
-        repo_name (str): The name of the Hugging Face Hub repository.
+        repo_id (str): The id of the Hugging Face Hub repository.
         trained_model_path (str): The local path to the fine-tuned model directory.
         push_model (bool): Whether to push the model. Default is True.
         push_tokenizer (bool): Whether to push the tokenizer. Default is True.
@@ -18,13 +18,13 @@ def push_to_hub(repo_name: str, trained_model_path: str, push_model: bool = Fals
     try:
         if push_tokenizer:
             tokenizer = AutoTokenizer.from_pretrained(trained_model_path)
-            tokenizer.push_to_hub(repo_name)
-            print(f"✓ Successfully pushed tokenizer to {repo_name}")
+            tokenizer.push_to_hub(repo_id)
+            print(f"✓ Successfully pushed tokenizer to {repo_id}")
 
         if push_model:
             model = AutoModelForCausalLM.from_pretrained(trained_model_path, trust_remote_code=True)
-            model.push_to_hub(repo_name)
-            print(f"✓ Successfully pushed model to {repo_name}")
+            model.push_to_hub(repo_id)
+            print(f"✓ Successfully pushed model to {repo_id}")
 
     except Exception as e:
         print(f"Error pushing to hub: {e}")
@@ -35,13 +35,13 @@ def parse_args():
     Parses command-line arguments.
     
     Returns:
-        argparse.Namespace: Parsed arguments containing repo_name and model_path.
+        argparse.Namespace: Parsed arguments containing repo_id and model_path.
     """
 
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--repo-name", type=str, required=True)
+    parser.add_argument("--repo-id", type=str, required=True)
     parser.add_argument("--trained-model-path", type=str, required=True)
     parser.add_argument("--push-model", type=bool)  
     parser.add_argument("--push-tokenizer", type=bool)  
@@ -52,4 +52,4 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    push_to_hub(args.repo_name, args.trained_model_path, args.push_model, args.push_tokenizer)
+    push_to_hub(args.repo_id, args.trained_model_path, args.push_model, args.push_tokenizer)
